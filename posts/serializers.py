5f1f3914b67f136439b42cd0ec3963462323c790
Simon Chanzu <chanzusimon6@gmail.com>
2025-03-23 18:56:31 +0300

    commits

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Post, Comment
+from users.serializers import UserSerializer  # assuming you have one
+
 
 class PostSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-
+    user = UserSerializer(read_only=True) 
     class Meta:
         model = Post
         fields = ['id', 'user', 'content', 'image', 'created_at']
--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,21 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('id', 'username', 'date_joined')
+    list_display = ('id', 'username', 'email', 'bio', 'phone', 'date_joined')
     search_fields = ('username', 'email')
     list_filter = ('is_active', 'is_staff', 'date_joined')
     ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'bio', 'profile_picture', 'phone')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'bio', 'profile_picture', 'phone'),
+        }),
+    )
